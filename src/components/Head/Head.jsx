a5fdeed7793765fc9a37a493ba3e30829427d866
Roman Ostrous <ostrous.roman@gmail.com>
2026-01-08 16:56:34 +0200

    add all assets files

---
+++ src/components/Head/Head.jsx
@@ -0,0 +1,13 @@
+import "./Head.css";
+
+export function Head() {
+  return (
+    <section className="head">
+      <p>
+        CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT) &nbsp |
+        &nbsp FREE SHIPPING on orders {">"} $200 {"\u00A0"} | {"\u00A0"} easy 45
+        day return window.
+      </p>
+    </section>
+  );
+}
